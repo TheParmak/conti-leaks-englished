843314e5d238fd479ebcc67852ec9ad624850812
Nathannaelc <65099536+Nathannaelc@users.noreply.github.com>
2022-03-01 09:06:05 +0700

    add more stuff to prox, move prox for fetching upstream

--- prox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sed -i '/^$/d' $1
-sed -i 's/{ "/"/g' $1
-sed -i 's/\\/{backslash}/g' $1
-sed -i 's/\"\./"/g' $1
-sed -i 's/},//g' $1
-sed -i -E 's/"body": "(.*)": "/"body": "\1 /g' $1
---
+++ scripts/prox.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sed -i '/^$/d' $1
+sed -i 's/{ "/"/g' $1
+sed -i 's/\\/{backslash}/g' $1
+sed -i 's/\"\./"/g' $1
+sed -i 's/},//g' $1
+sed -i 's/   "/  "/g' $1
+sed -i 's/")\n/)"\n/g' $1
+sed -i -E 's/"body": "(.*)": "/"body": "\1 /g' $1
+sed -i -E 's/"from": "(.*)"\n/"from": "\1",\n/g' $1
