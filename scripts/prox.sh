3fd77aba625741edce3d0ea58a05c89f0863b566
Nathannaelc <65099536+Nathannaelc@users.noreply.github.com>
2022-03-01 11:37:35 +0700

    Update prox and run recursive_prox to v1

--- scripts/prox.sh
+++ scripts/prox.sh
@@ -6,5 +6,6 @@ sed -i 's/\"\./"/g' $1
 sed -i 's/},//g' $1
 sed -i 's/   "/  "/g' $1
 sed -i 's/")\n/)"\n/g' $1
-sed -i -E 's/"body": "(.*)": "/"body": "\1 /g' $1
+sed -i 's/"?\n/?"\n/g' $1
+sed -i -E 's/"body": "(.*)": "/"body": "\1 : /g' $1
 sed -i -E 's/"from": "(.*)"\n/"from": "\1",\n/g' $1
