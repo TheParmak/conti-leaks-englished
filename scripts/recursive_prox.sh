0dd7cbdc0a2cc0529682e9163cd01502796d67de
Nathannaelc <65099536+Nathannaelc@users.noreply.github.com>
2022-03-01 11:14:56 +0700

    Update recursive_prox and prox
    change line endings to UNIX, run recursive_prox to v1/deepl/json (terminated early)

--- scripts/recursive_prox.sh
+++ scripts/recursive_prox.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-FILES='file_path'
-
-for f in $FILES
-do
-	echo "Processing $f file..."
-	# take action on each file. $f store current file name
-	sed '/^$/d' "$f" > "$f.2.json"
-	cat "$f.2.json" | sed 's/{ "/"/g' > "$f.3.json"
-	cat "$f.3.json" | sed 's/\\/{backslash}/g' > "$f.4.json"
-	cat "$f.4.json" | sed 's/\"\./"/g' > "$f.5.json"
-	cat "$f.5.json" | sed 's/},//g' > "$f"
-	rm -rf "$f.2.json" "$f.3.json" "$f.4.json" "$f.5.json"
-
+pwd=$(pwd)
+cd $(echo $1 | tr -d '\r')
+for f in *; do
+  echo "Processing $f file..."
+  bash $pwd/prox.sh $f
 done
-
+cd $(echo $pwd | tr -d '\r')
