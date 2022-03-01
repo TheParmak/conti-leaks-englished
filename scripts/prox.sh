21b2970d098a1ddb4720732af43c8ba24776e474
Nathannaelc <65099536+Nathannaelc@users.noreply.github.com>
2022-03-01 09:06:05 +0700

    add more stuff to prox, move prox for fetching upstream

--- scripts/prox.sh
+++ scripts/prox.sh
@@ -4,4 +4,11 @@ sed -i 's/{ "/"/g' $1
 sed -i 's/\\/{backslash}/g' $1
 sed -i 's/\"\./"/g' $1
 sed -i 's/},//g' $1
+<<<<<<< HEAD
 sed -i -E 's/"body": "(.*)": "/"body": "\1 /g' $1
+=======
+sed -i 's/   "/  "/g' $1
+sed -i 's/")\n/)"\n/g' $1
+sed -i -E 's/"body": "(.*)": "/"body": "\1 /g' $1
+sed -i -E 's/"from": "(.*)"\n/"from": "\1",\n/g' $1
+>>>>>>> 843314e (add more stuff to prox, move prox for fetching upstream)
